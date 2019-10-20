20e45c0a849d401cac3c05fd00d320ff7c85fe8d
Theodosios Tziomakas <theoaristi53@gmail.com>
2019-10-20 13:38:11 +0300

    NPM Scripts Part 1

---
+++ conFusion/js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    // $("#mycarousel").carousel( { interval: 2000 } );
+    // $("#carousel-pause").click(function(){
+
+    // });
+    $('#mycarousel').carousel({interval: 2000 });
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+});
+$("#reservationButton").click(function(){
+    $('#reservationModal').modal('show')
+});
+$("#loginButton").click(function(){
+    $('#loginModal').modal('show')
+});
